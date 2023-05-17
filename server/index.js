e9993788e33ce3969499c7415fe14694bc00bdfc
haylin <eileenza@protonmail.com>
2023-05-16 22:48:22 -0400

    created psql schema & testing first route

--- server/index.js
+++ server/index.js
@@ -2,11 +2,30 @@ const express = require('express');
 const app = express();
 
 const cors = require('cors');
+const pool = require('./db');
 
 //Middleware - Allow frontend & backed to connect
 app.use(cors())
+app.use(express.json())
 
 let PORT  = 5000 
+
+//GET all todos
+
+//GET one todo
+
+//CREATE one todo
+app.post('/todos', async (req, res) => {
+    try {
+        res.json(req.body);
+    } catch (e) {
+        console.error(e.message);
+    }
+})
+
+//UPDATE a todo
+
+//DELETE one todo
 
 app.listen(PORT, () =>{
     console.log(`server is starting on port ${PORT}`);
