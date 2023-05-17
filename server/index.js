55b5a0b0b73975ea7c0c017b28c3ba59a0d3615c
haylin <eileenza@protonmail.com>
2023-05-17 10:25:31 -0400

    created another route & server returns static frontend (only html at this point)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const cors = require('cors');
 const pool = require('./db');
+const path = require('path')
 
 //Middleware - Allow frontend & backed to connect
 app.use(cors())
@@ -10,15 +11,32 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
+//we are getting a static html file
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+//return react frontend app
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../client/index.html'))
+})
+
 //GET all todos
 
 //GET one todo
+app.get('/todos', async (req, res) => {
+    try {
+        const description = req.body;
+        const allTodos = await pool.query("SELECT * FROM list; ")
+        res.json(allTodos.rows);
+    } catch (e) {
+        console.error(e.message);
+    } 
+})
 
 //CREATE one todo
 app.post('/todos', async (req, res) => {
     try {
         const description = req.body;
-        const newTodo = await pool.query("INSERT INTO list (description) VALUES ($1) RETURNING * "
+        const newTodo = await pool.query("INSERT INTO list (description) VALUES ($1) RETURNING * ;"
         , [description])
         res.json(newTodo.rows[0]);
     } catch (e) {
