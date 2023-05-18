9b21f42728d1401af2e92f604c4bceaaed139176
haylin <eileenza@protonmail.com>
2023-05-18 19:09:17 -0400

    worked on the Input component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Input';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +16,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
---
+++ client/src/components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Input  = () => {
+    return (
+        <h1>Input Todo</h1>
+    )
+}
+
+export default Input;
